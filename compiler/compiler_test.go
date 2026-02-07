37ee8f10db5e5e828dae8e0b9bfe1917dfb2e593
Kyle Hickman <kyle@txhickman.com>
2026-02-06 21:57:37 -0600

    prefix ops

--- compiler/compiler_test.go
+++ compiler/compiler_test.go
@@ -69,6 +69,15 @@ func TestIntegerArithmetic(t *testing.T) {
 				code.Make(code.OpPop),
 			},
 		},
+		{
+			input:             "-1",
+			expectedConstants: []interface{}{1},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 0),
+				code.Make(code.OpMinus),
+				code.Make(code.OpPop),
+			},
+		},
 	}
 
 	runCompilerTests(t, tests)
@@ -76,6 +85,15 @@ func TestIntegerArithmetic(t *testing.T) {
 
 func TestBooleanExpressions(t *testing.T) {
 	tests := []compilerTestCase{
+		{
+			input:             "!true",
+			expectedConstants: []interface{}{},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpTrue),
+				code.Make(code.OpBang),
+				code.Make(code.OpPop),
+			},
+		},
 		{
 			input:             "1 > 2",
 			expectedConstants: []interface{}{1, 2},
