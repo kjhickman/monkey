3853a99bbb74d77d1f02e84584c7ea1fdf5cea14
Kyle Hickman <kyle@txhickman.com>
2026-02-25 16:47:31 -0600

    add tests and support for more infix operators

--- src/Kong/CodeGeneration/IlCompiler.cs
+++ src/Kong/CodeGeneration/IlCompiler.cs
@@ -38,12 +38,36 @@ public class IlCompiler
                 il.Emit(OpCodes.Ldc_I8, intLit.Value);
                 return null;
 
-            case InfixExpression infix when infix.Operator == "+":
-                var leftErr = EmitExpression(infix.Left, types, il, module);
+            case InfixExpression plus when plus.Operator == "+":
+                var leftErr = EmitExpression(plus.Left, types, il, module);
                 if (leftErr is not null) return leftErr;
-                var rightErr = EmitExpression(infix.Right, types, il, module);
+                var rightErr = EmitExpression(plus.Right, types, il, module);
                 if (rightErr is not null) return rightErr;
                 il.Emit(OpCodes.Add);
+                return null;
+            
+            case InfixExpression minus when minus.Operator == "-":
+                var leftErr2 = EmitExpression(minus.Left, types, il, module);
+                if (leftErr2 is not null) return leftErr2;
+                var rightErr2 = EmitExpression(minus.Right, types, il, module);
+                if (rightErr2 is not null) return rightErr2;
+                il.Emit(OpCodes.Sub);
+                return null;
+
+            case InfixExpression times when times.Operator == "*":
+                var leftErr3 = EmitExpression(times.Left, types, il, module);
+                if (leftErr3 is not null) return leftErr3;
+                var rightErr3 = EmitExpression(times.Right, types, il, module);
+                if (rightErr3 is not null) return rightErr3;
+                il.Emit(OpCodes.Mul);
+                return null;
+
+            case InfixExpression div when div.Operator == "/":
+                var leftErr4 = EmitExpression(div.Left, types, il, module);
+                if (leftErr4 is not null) return leftErr4;
+                var rightErr4 = EmitExpression(div.Right, types, il, module);
+                if (rightErr4 is not null) return rightErr4;
+                il.Emit(OpCodes.Div);
                 return null;
 
             default:
---
+++ tests/Kong.Tests/CodeGeneration/IlCompilerIntegrationTests.cs
@@ -0,0 +1,90 @@
+using System.Diagnostics;
+using Kong.CodeGeneration;
+using Kong.Lexing;
+using Kong.Parsing;
+using Kong.Evaluating;
+
+namespace Kong.Tests.CodeGeneration;
+
+public class IlCompilerIntegrationTests
+{
+    public static TheoryData<string> ArithmeticPrograms =>
+    [
+        "1",
+        "1 + 2",
+        "1 - 2",
+        "1 * 2",
+        "4 / 2",
+    ];
+
+    [Theory]
+    [MemberData(nameof(ArithmeticPrograms))]
+    public async Task ClrBackend_MatchesVm_ForStarterArithmeticMatrix(string source)
+    {
+        var vmOutput = RunOnVm(source);
+        var clrOutput = await CompileAndRunOnClr(source);
+
+        Assert.Equal(vmOutput, clrOutput);
+    }
+
+    private static string RunOnVm(string source)
+    {
+        var lexer = new Lexer(source);
+        var parser = new Parser(lexer);
+        var program = parser.ParseProgram();
+
+        var parserErrors = parser.Errors();
+        Assert.Empty(parserErrors);
+
+        var compiler = new Compiler();
+        var compileError = compiler.Compile(program);
+        Assert.Null(compileError);
+
+        var vm = new Vm(compiler.GetBytecode());
+        var vmError = vm.Run();
+        Assert.Null(vmError);
+
+        return vm.LastPoppedStackElem().Inspect();
+    }
+
+    private static async Task<string> CompileAndRunOnClr(string source)
+    {
+        var tempDirectory = Path.Combine(Path.GetTempPath(), $"kong-il-tests-{Guid.NewGuid():N}");
+        Directory.CreateDirectory(tempDirectory);
+
+        try
+        {
+            var assemblyPath = Path.Combine(tempDirectory, "program.dll");
+            var compiler = new KongCompiler();
+            var compileError = compiler.CompileToAssembly(source, "program", assemblyPath);
+            Assert.Null(compileError);
+
+            var startInfo = new ProcessStartInfo
+            {
+                FileName = "dotnet",
+                Arguments = $"\"{assemblyPath}\"",
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+            };
+
+            using var process = Process.Start(startInfo);
+            Assert.NotNull(process);
+
+            var stdOut = await process.StandardOutput.ReadToEndAsync();
+            var stdErr = await process.StandardError.ReadToEndAsync();
+
+            await process.WaitForExitAsync();
+            Assert.True(process.ExitCode == 0, $"dotnet exited with code {process.ExitCode}: {stdErr}");
+
+            return stdOut.TrimEnd();
+        }
+        finally
+        {
+            if (Directory.Exists(tempDirectory))
+            {
+                Directory.Delete(tempDirectory, recursive: true);
+            }
+        }
+    }
+}
