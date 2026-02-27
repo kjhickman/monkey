a2dd6e6a106bce9a3aed58616988f44fbc6748c5
Kyle Hickman <kyle@txhickman.com>
2026-02-27 02:43:37 -0600

    refactor type inferer

--- src/Kong/Semantics/TypeInferer.cs
+++ src/Kong/Semantics/TypeInferer.cs
@@ -31,180 +31,36 @@ public class TypeInferer
             lastType = InferNode(statement, result, env);
         }
 
-        result.AddNodeType(program, lastType);
-        return lastType;
+        return SetType(program, lastType, result);
     }
 
     private KongType InferStatement(IStatement statement, TypeInferenceResult result, Dictionary<string, KongType> env)
     {
-        switch (statement)
+        return statement switch
         {
-            case ExpressionStatement es when es.Expression is not null:
-                var expressionType = InferExpression(es.Expression, result, env);
-                result.AddNodeType(statement, expressionType);
-                return expressionType;
-
-            case LetStatement ls when ls.Value is not null:
-                var valueType = InferExpression(ls.Value, result, env);
-                if (valueType == KongType.Void)
-                {
-                    result.AddError("Type error: cannot assign an expression with no value");
-                    result.AddNodeType(ls.Name, KongType.Unknown);
-                    result.AddNodeType(statement, KongType.Unknown);
-                    return KongType.Unknown;
-                }
-                env[ls.Name.Value] = valueType;
-                result.AddNodeType(ls.Name, valueType);
-                result.AddNodeType(statement, valueType);
-                return valueType;
-
-            case BlockStatement bs:
-                var lastType = KongType.Unknown;
-                foreach (var stmt in bs.Statements)
-                {
-                    lastType = InferNode(stmt, result, env);
-                }
-                result.AddNodeType(statement, lastType);
-                return lastType;
-            
-            default:
-                result.AddError($"Unsupported statement type: {statement.GetType().Name}");
-                result.AddNodeType(statement, KongType.Unknown);
-                return KongType.Unknown;
-        }
+            ExpressionStatement es when es.Expression is not null => InferExpressionStatement(es, result, env),
+            LetStatement ls when ls.Value is not null => InferLetStatement(ls, result, env),
+            BlockStatement bs => InferBlockStatement(bs, result, env),
+            _ => AddErrorAndSetType(statement, $"Unsupported statement type: {statement.GetType().Name}", KongType.Unknown, result),
+        };
     }
 
     private KongType InferExpression(IExpression expression, TypeInferenceResult result, Dictionary<string, KongType> env)
     {
-        switch (expression)
+        return expression switch
         {
-            case IntegerLiteral:
-                result.AddNodeType(expression, KongType.Int64);
-                return KongType.Int64;
-
-            case BooleanLiteral:
-                result.AddNodeType(expression, KongType.Boolean);
-                return KongType.Boolean;
-
-            case StringLiteral:
-                result.AddNodeType(expression, KongType.String);
-                return KongType.String;
-
-            case ArrayLiteral arrayLit:
-                if (arrayLit.Elements.Count == 0)
-                {
-                    result.AddNodeType(expression, KongType.Array);
-                    return KongType.Array;
-                }
-                var elementType = InferExpression(arrayLit.Elements[0], result, env);
-                foreach (var elem in arrayLit.Elements.Skip(1))
-                {
-                    var currentType = InferExpression(elem, result, env);
-                    if (currentType != elementType)
-                    {
-                        result.AddError($"Type error: array elements must have the same type, but found {elementType} and {currentType}");
-                        result.AddNodeType(expression, KongType.Unknown);
-                        return KongType.Unknown;
-                    }
-                }
-                result.AddNodeType(expression, KongType.Array);
-                return KongType.Array;
-
-            case InfixExpression infix:
-                return InferInfix(infix, result, env);
-
-            case Identifier identifier:
-                if (env.TryGetValue(identifier.Value, out var identType))
-                {
-                    result.AddNodeType(expression, identType);
-                    return identType;
-                }
-                result.AddError($"Undefined variable: {identifier.Value}");
-                result.AddNodeType(expression, KongType.Unknown);
-                return KongType.Unknown;
-            
-            case PrefixExpression prefix when prefix.Operator == "-":
-                var rightType = InferExpression(prefix.Right, result, env);
-                if (rightType != KongType.Int64)
-                {
-                    result.AddError($"Type error: cannot apply operator '-' to type {rightType}");
-                    result.AddNodeType(expression, KongType.Unknown);
-                    return KongType.Unknown;
-                }
-                result.AddNodeType(expression, KongType.Int64);
-                return KongType.Int64;
-
-            case PrefixExpression prefix when prefix.Operator == "!":
-                var operandType = InferExpression(prefix.Right, result, env);
-                if (operandType != KongType.Boolean)
-                {
-                    result.AddError($"Type error: cannot apply operator '!' to type {operandType}");
-                    result.AddNodeType(expression, KongType.Unknown);
-                    return KongType.Unknown;
-                }
-                result.AddNodeType(expression, KongType.Boolean);
-                return KongType.Boolean;
-
-            case IfExpression ifExpr:
-                var conditionType = InferExpression(ifExpr.Condition, result, env);
-                if (conditionType != KongType.Boolean)
-                {
-                    result.AddError($"Type error: if condition must be of type Boolean, but got {conditionType}");
-                    result.AddNodeType(expression, KongType.Unknown);
-                    return KongType.Unknown;
-                }
-
-                var thenType = InferStatement(ifExpr.Consequence, result, env);
-
-                if (ifExpr.Alternative is null)
-                {
-                    result.AddNodeType(expression, KongType.Void);
-                    return KongType.Void;
-                }
-                var elseType = InferStatement(ifExpr.Alternative, result, env);
-
-                if (thenType != elseType)
-                {
-                    result.AddError($"Type error: if branches must have the same type, but got {thenType} and {elseType}");
-                    result.AddNodeType(expression, KongType.Unknown);
-                    return KongType.Unknown;
-                }
-
-                if (thenType == KongType.Void)
-                {
-                    result.AddError("Type error: if/else used as an expression must produce a value");
-                    result.AddNodeType(expression, KongType.Unknown);
-                    return KongType.Unknown;
-                }
-
-                result.AddNodeType(expression, thenType);
-                return thenType;
-
-            case IndexExpression indexExpr:
-                var leftType = InferExpression(indexExpr.Left, result, env);
-                if (leftType is not KongType.Array and not KongType.Unknown)
-                {
-                    result.AddError($"Type error: index operator not supported for type {leftType}");
-                    result.AddNodeType(expression, KongType.Unknown);
-                    return KongType.Unknown;
-                }
-
-                var indexType = InferExpression(indexExpr.Index, result, env);
-                if (indexType is not KongType.Int64 and not KongType.Unknown)
-                {
-                    result.AddError($"Type error: array index must be Int64, but got {indexType}");
-                    result.AddNodeType(expression, KongType.Unknown);
-                    return KongType.Unknown;
-                }
-
-                result.AddNodeType(expression, KongType.Unknown);
-                return KongType.Unknown;
-
-            default:
-                result.AddError($"Unsupported expression: {expression.GetType().Name}");
-                result.AddNodeType(expression, KongType.Unknown);
-                return KongType.Unknown;
-        }
+            IntegerLiteral => SetType(expression, KongType.Int64, result),
+            BooleanLiteral => SetType(expression, KongType.Boolean, result),
+            StringLiteral => SetType(expression, KongType.String, result),
+            ArrayLiteral arrayLit => InferArrayLiteral(arrayLit, result, env),
+            InfixExpression infix => InferInfix(infix, result, env),
+            Identifier identifier => InferIdentifier(identifier, result, env),
+            PrefixExpression prefix when prefix.Operator == "-" => InferNegationPrefix(prefix, result, env),
+            PrefixExpression prefix when prefix.Operator == "!" => InferBangPrefix(prefix, result, env),
+            IfExpression ifExpr => InferIfExpression(ifExpr, result, env),
+            IndexExpression indexExpr => InferIndexExpression(indexExpr, result, env),
+            _ => AddErrorAndSetType(expression, $"Unsupported expression: {expression.GetType().Name}", KongType.Unknown, result),
+        };
     }
 
     private KongType InferInfix(InfixExpression infix, TypeInferenceResult result, Dictionary<string, KongType> env)
@@ -216,56 +72,182 @@ public class TypeInferer
         {
             if (leftType == KongType.String && rightType == KongType.String)
             {
-                result.AddNodeType(infix, KongType.String);
-                return KongType.String;
+                return SetType(infix, KongType.String, result);
             }
 
             if (leftType == KongType.Int64 && rightType == KongType.Int64)
             {
-                result.AddNodeType(infix, KongType.Int64);
-                return KongType.Int64;
+                return SetType(infix, KongType.Int64, result);
             }
 
-            result.AddError($"Type error: cannot apply operator '+' to types {leftType} and {rightType}");
-            result.AddNodeType(infix, KongType.Unknown);
-            return KongType.Unknown;
+            return AddErrorAndSetType(infix, $"Type error: cannot apply operator '+' to types {leftType} and {rightType}", KongType.Unknown, result);
         }
         
         if (infix.Operator is "-" or "*" or "/")
         {
             if (leftType != KongType.Int64 || rightType != KongType.Int64)
             {
-                result.AddError($"Type error: cannot apply operator '{infix.Operator}' to types {leftType} and {rightType}");
-                result.AddNodeType(infix, KongType.Unknown);
-                return KongType.Unknown;
+                return AddErrorAndSetType(infix, $"Type error: cannot apply operator '{infix.Operator}' to types {leftType} and {rightType}", KongType.Unknown, result);
             }
 
-            result.AddNodeType(infix, KongType.Int64);
-            return KongType.Int64;
+            return SetType(infix, KongType.Int64, result);
         }
 
         if (infix.Operator is "==" or "!=" or "<" or ">")
         {
             if (leftType != rightType)
             {
-                result.AddError($"Type error: cannot compare types {leftType} and {rightType} with operator '{infix.Operator}'");
-                result.AddNodeType(infix, KongType.Unknown);
-                return KongType.Unknown;
+                return AddErrorAndSetType(infix, $"Type error: cannot compare types {leftType} and {rightType} with operator '{infix.Operator}'", KongType.Unknown, result);
             }
 
-            result.AddNodeType(infix, KongType.Boolean);
-            return KongType.Boolean;
+            return SetType(infix, KongType.Boolean, result);
         }
         
-        result.AddError($"Type error: cannot apply operator '{infix.Operator}' to types {leftType} and {rightType}");
-        result.AddNodeType(infix, KongType.Unknown);
-        return KongType.Unknown;
+        return AddErrorAndSetType(infix, $"Type error: cannot apply operator '{infix.Operator}' to types {leftType} and {rightType}", KongType.Unknown, result);
+    }
+
+    private KongType InferExpressionStatement(ExpressionStatement expressionStatement, TypeInferenceResult result, Dictionary<string, KongType> env)
+    {
+        var expressionType = InferExpression(expressionStatement.Expression!, result, env);
+        return SetType(expressionStatement, expressionType, result);
+    }
+
+    private KongType InferLetStatement(LetStatement letStatement, TypeInferenceResult result, Dictionary<string, KongType> env)
+    {
+        var valueType = InferExpression(letStatement.Value!, result, env);
+        if (valueType == KongType.Void)
+        {
+            SetType(letStatement.Name, KongType.Unknown, result);
+            return AddErrorAndSetType(letStatement, "Type error: cannot assign an expression with no value", KongType.Unknown, result);
+        }
+
+        env[letStatement.Name.Value] = valueType;
+        SetType(letStatement.Name, valueType, result);
+        return SetType(letStatement, valueType, result);
+    }
+
+    private KongType InferBlockStatement(BlockStatement block, TypeInferenceResult result, Dictionary<string, KongType> env)
+    {
+        var lastType = KongType.Unknown;
+        foreach (var statement in block.Statements)
+        {
+            lastType = InferNode(statement, result, env);
+        }
+
+        return SetType(block, lastType, result);
+    }
+
+    private KongType InferArrayLiteral(ArrayLiteral arrayLiteral, TypeInferenceResult result, Dictionary<string, KongType> env)
+    {
+        if (arrayLiteral.Elements.Count == 0)
+        {
+            return SetType(arrayLiteral, KongType.Array, result);
+        }
+
+        var elementType = InferExpression(arrayLiteral.Elements[0], result, env);
+        foreach (var element in arrayLiteral.Elements.Skip(1))
+        {
+            var currentType = InferExpression(element, result, env);
+            if (currentType != elementType)
+            {
+                return AddErrorAndSetType(arrayLiteral, $"Type error: array elements must have the same type, but found {elementType} and {currentType}", KongType.Unknown, result);
+            }
+        }
+
+        return SetType(arrayLiteral, KongType.Array, result);
+    }
+
+    private KongType InferIdentifier(Identifier identifier, TypeInferenceResult result, Dictionary<string, KongType> env)
+    {
+        if (env.TryGetValue(identifier.Value, out var identifierType))
+        {
+            return SetType(identifier, identifierType, result);
+        }
+
+        return AddErrorAndSetType(identifier, $"Undefined variable: {identifier.Value}", KongType.Unknown, result);
+    }
+
+    private KongType InferNegationPrefix(PrefixExpression prefix, TypeInferenceResult result, Dictionary<string, KongType> env)
+    {
+        var rightType = InferExpression(prefix.Right, result, env);
+        if (rightType != KongType.Int64)
+        {
+            return AddErrorAndSetType(prefix, $"Type error: cannot apply operator '-' to type {rightType}", KongType.Unknown, result);
+        }
+
+        return SetType(prefix, KongType.Int64, result);
+    }
+
+    private KongType InferBangPrefix(PrefixExpression prefix, TypeInferenceResult result, Dictionary<string, KongType> env)
+    {
+        var operandType = InferExpression(prefix.Right, result, env);
+        if (operandType != KongType.Boolean)
+        {
+            return AddErrorAndSetType(prefix, $"Type error: cannot apply operator '!' to type {operandType}", KongType.Unknown, result);
+        }
+
+        return SetType(prefix, KongType.Boolean, result);
+    }
+
+    private KongType InferIfExpression(IfExpression ifExpression, TypeInferenceResult result, Dictionary<string, KongType> env)
+    {
+        var conditionType = InferExpression(ifExpression.Condition, result, env);
+        if (conditionType != KongType.Boolean)
+        {
+            return AddErrorAndSetType(ifExpression, $"Type error: if condition must be of type Boolean, but got {conditionType}", KongType.Unknown, result);
+        }
+
+        var thenType = InferStatement(ifExpression.Consequence, result, env);
+        if (ifExpression.Alternative is null)
+        {
+            return SetType(ifExpression, KongType.Void, result);
+        }
+
+        var elseType = InferStatement(ifExpression.Alternative, result, env);
+        if (thenType != elseType)
+        {
+            return AddErrorAndSetType(ifExpression, $"Type error: if branches must have the same type, but got {thenType} and {elseType}", KongType.Unknown, result);
+        }
+
+        if (thenType == KongType.Void)
+        {
+            return AddErrorAndSetType(ifExpression, "Type error: if/else used as an expression must produce a value", KongType.Unknown, result);
+        }
+
+        return SetType(ifExpression, thenType, result);
+    }
+
+    private KongType InferIndexExpression(IndexExpression indexExpression, TypeInferenceResult result, Dictionary<string, KongType> env)
+    {
+        var leftType = InferExpression(indexExpression.Left, result, env);
+        if (leftType is not KongType.Array and not KongType.Unknown)
+        {
+            return AddErrorAndSetType(indexExpression, $"Type error: index operator not supported for type {leftType}", KongType.Unknown, result);
+        }
+
+        var indexType = InferExpression(indexExpression.Index, result, env);
+        if (indexType is not KongType.Int64 and not KongType.Unknown)
+        {
+            return AddErrorAndSetType(indexExpression, $"Type error: array index must be Int64, but got {indexType}", KongType.Unknown, result);
+        }
+
+        return SetType(indexExpression, KongType.Unknown, result);
+    }
+
+    private static KongType SetType(INode node, KongType type, TypeInferenceResult result)
+    {
+        result.AddNodeType(node, type);
+        return type;
+    }
+
+    private static KongType AddErrorAndSetType(INode node, string error, KongType type, TypeInferenceResult result)
+    {
+        result.AddError(error);
+        return SetType(node, type, result);
     }
 
     private KongType InferUnsupported(INode node, TypeInferenceResult result)
     {
-        result.AddError($"Unsupported node type: {node.GetType().Name}");
-        result.AddNodeType(node, KongType.Unknown);
-        return KongType.Unknown;
+        return AddErrorAndSetType(node, $"Unsupported node type: {node.GetType().Name}", KongType.Unknown, result);
     }
 }
